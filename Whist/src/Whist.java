f85182dbb5b6b94087c7edf60174d46ef5b52e32
hastalapasta <dries.de.backker@gmail.com>
2016-09-16 17:15:38 +0200

    whist file eindelijk

---
+++ Whist/src/Whist.java
@@ -0,0 +1,10 @@
+import whist.gui.WhistGUI;
+import whist.model.Facade;
+
+public class Whist {
+
+	public static void main(String[] args) {
+		WhistGUI gui = new WhistGUI(new Facade());
+		gui.start();
+	}
+}
